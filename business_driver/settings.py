cd8e196d3947a88d8c2c16eca0b6f4686bb00459
jjavierl <sistemasuatf12345@gmail.com>
2021-10-30 01:36:04 -0400

    configuracion de postgresql

--- business_driver/settings.py
+++ business_driver/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'business_driver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'business',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
